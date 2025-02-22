234933399df008fb1e5c8932a103e3cf315bae3a
Qwqwww <jollot@yandex.ru>
2025-02-22 18:39:23 +0700

    Добавлен HTML-файл с разметкой

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/hello')
 def hello():
-    return "<h1>Hello!</h1><p>Text</p>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
