097c5ad592e5316bd1ff830b9a3fa841c074aa86
Qwqwww <jollot@yandex.ru>
2025-02-22 18:52:07 +0700

    Интегрирована модель и сообщение о результатах работы модели

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=["get", "post"])
 def hello():
+    message = ""
     if request.method == "POST":
         area = request.form.get("area")
+        # TODO: Добавить проверку ввода
         area = float(area)
         cost = get_prediction(area)
-        print(cost)
-        
-    return render_template("index.html")
+        message = f"Стоимость квартиры площадью {area} кв.м. равна {cost} рублей."
+    return render_template("index.html", message=message)
 
 
 if __name__ == "__main__":
