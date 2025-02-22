81cb05f1d62be7bf01e9e61f77f95fbaec0e1a10
Qwqwww <jollot@yandex.ru>
2025-02-22 19:03:12 +0700

    Добавлена проверка ввода

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ def hello():
     message = ""
     if request.method == "POST":
         area = request.form.get("area")
-        # TODO: Добавить проверку ввода
-        area = float(area)
+        try:
+            area = float(area)
+        except Exception as e:
+            print(e)
+            message += "Некорректный ввод. Установлено значение по умолчанию: 0 кв.м."
+            area = 0.0
         cost = get_prediction(area)
-        message = f"Стоимость квартиры площадью {area} кв.м. равна {cost} рублей."
+        message += f"Стоимость квартиры площадью {area} кв.м. равна {cost} рублей."
     return render_template("index.html", message=message)
 
