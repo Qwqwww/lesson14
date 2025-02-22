eb6b38e20ea40eceae52e5691565c64f0d63379e
Qwqwww <jollot@yandex.ru>
2025-02-22 18:45:26 +0700

    Добавлена форма для ввода площади

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/', methods=["get", "post"])
 def hello():
+    if request.method == "POST":
+        area = request.form.get("area")
+        print(area)
     return render_template("index.html")
 
 
